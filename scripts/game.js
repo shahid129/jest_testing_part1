dd20b727b9dd40d388d711203d83f6ced2d5d218
Shahidul Islam <shahid.okay@gmail.com>
2022-07-16 15:09:35 +0000

    Add more test function along with game function

--- scripts/game.js
+++ scripts/game.js
@@ -1,7 +1,8 @@
 let game = {
-    score : 0,
-    currentGame : [],
-    playerMoves : [],
+    score: 0,
+    currentGame: [],
+    playerMoves: [],
+    turnNumber: 0,
     choices: ['button1', "button2", "button3", "button4"],
 };
 
@@ -9,6 +10,17 @@ function newGame() {
     game.score = 0;
     game.currentGame = [];
     game.playerMoves = [];
+    for (let circle of document.getElementsByClassName("circle")) {
+        if (circle.getAttribute("data-listener") !== "true"){
+            circle.addEventListener("click", (e) => {
+                let move = e.target.getAttribute("id");
+                lightsOn(move);
+                game.playerMoves.push(move);
+                playerTurn();
+            });
+            circle.setAttribute("data-listener", "true");
+        }
+    }
     showScore();
     addTurn();
 }
@@ -16,12 +28,37 @@ function newGame() {
 function addTurn() {
     game.playerMoves = [];
     game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
-    // showTurns()
+    showTurns()
 }
 
 function showScore() {
     document.getElementById("score").innerText = game.score;
 }
 
+function lightsOn(circ) {
+    document.getElementById(circ).classList.add("light");
+    setTimeout(() => {
+        document.getElementById(circ).classList.remove("light");
+    }, 400);
+}
 
-module.exports = { game, newGame, showScore, addTurn }
+function showTurns() {
+    game.turnNumber = 0;
+    let turns = setInterval(() => {
+        lightsOn(game.currentGame[game.turnNumber]);
+        game.turnNumber++;
+        if (game.turnNumber >= game.currentGame.length) {
+            clearInterval(turns);
+        }
+    }, 800)
+}
+
+
+module.exports = {
+    game,
+    newGame,
+    showScore,
+    addTurn,
+    lightsOn,
+    showTurns
+}
--- scripts/test/game.test.js
+++ scripts/test/game.test.js
@@ -1,5 +1,5 @@
 
-const { game, newGame, showScore, addTurn } = require("../game");
+const { game, newGame, showScore, addTurn, lightsOn, showTurns } = require("../game");
 
 
 beforeAll(() => {
@@ -26,6 +26,9 @@ describe("game object contains correct keys", () => {
     test("choices contain correct ids", () => {
         expect(game.choices).toEqual(['button1', "button2", "button3", "button4"]);
     });
+    test("turnNumber key exists", () => {
+        expect("turnNumber" in game).toBe(true);
+    });
 });
 
 describe("newGame works correctly", () => {
@@ -49,3 +52,37 @@ describe("newGame works correctly", () => {
         expect(document.getElementById("score").innerText).toEqual(0);
     });
 })
+
+describe("gameplay works correctly", () => {
+    beforeEach(() => {
+        game.score = 0;
+        game.currentGame = [];
+        game.playerMoves = [];
+        addTurn();
+    })
+    afterEach(() => {
+        game.score = 0;
+        game.currentGame = [];
+        game.playerMoves = [];
+    })
+    test("addTurn adds a new turn to the game", () => {
+        addTurn();
+        expect(game.currentGame.length).toBe(2);
+    });
+    test("shoud add correct class to light up the buttons", () =>{
+        let button = document.getElementById(game.currentGame[0]);
+        lightsOn(game.currentGame[0]);
+        expect(button.classList).toContain("light");
+    });
+    test("showTurns should update game.turnNumber", () =>{
+        game.turnNumber = 42;
+        showTurns();
+        expect(game.turnNumber).toBe(0);
+    });
+    test("expect data-listener to be true", () =>{
+        const elements = document.getElementsByClassName("circle");
+        for (element of elements) {
+            expect(element.getAttribute("data-listener")).toEqual("true");
+        };
+    });
+});
